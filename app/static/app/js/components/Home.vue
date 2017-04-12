c133f5aaf5d3bed86dc9291f6a11cc8de221671f
domazet93 <toni.domaz93@hotmail.com>
2017-04-12 11:54:16 +0200

    design tweaks for content
    add footer component
    
    footer bar tweaks and contact form refactoring

--- app/static/app/js/components/Home.vue
+++ app/static/app/js/components/Home.vue
@@ -1,34 +1,39 @@
 <template>
     <div id="home">
-        <div class="greeting">
-            <div class="row">
-               <h2>Hello world</h2>
-            </div>
-            <div class="row">
-                <p>Thinking of what to write in a greeting card can be hard work!</p>
-            </div>
-             <div class="row">
-                <img src="~images/avatar.png">
-            </div>
-        </div>
-        <div class="row">
+        <div class="row text-center">
             <div class="col-md-4 col-md-offset-4">
                 <navigation></navigation>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-4 col-md-offset-2">
-                <h2>First</h2>
-                <p>I've tried this for both row, and span12, but my table inside span12, inside row always sits left. Adding padding moves it to the center, but I want to center it in a responsive way. Can I do auto padding somehow?</p>
+        <div class="content">
+            <div class="row mb-2">
+                <div class="col-md-4 border-light">
+                    <h2>First</h2>
+                    <p>I've tried this for both row, and span12, but my table inside span12, inside row always sits left. Adding padding moves it to the center, but I want to center it in a responsive way.<br><br>
+
+                    Can I do auto padding somehow?I've tried this for both row, and span12, but my table inside span12, inside row always sits left. Adding padding moves it to the center, but I want to center it in a responsive way. Can I do auto padding somehow</p>
+                </div>
+
+                <div class="col-md-4">
+                    <img src="~images/development.jpg" class="img-development">
+                </div>
+
+                <div class="col-md-4 border-light">
+                    <h2>First</h2>
+                    <p>I've tried this for both row, and span12, but my table inside span12, inside row always sits left. Adding padding moves it to the center, but I want to center it in a responsive way. Can I do auto padding somehow?</p>
+                </div>
             </div>
-            <div class="col-md-4">
-                <h2>First</h2>
-                <p>I've tried this for both row, and span12, but my table inside span12, inside row always sits left. Adding padding moves it to the center, but I want to center it in a responsive way. Can I do auto padding somehow?</p>
+            <div class="row">
+                <div class="col-md-4 border-light">
+                    <h2>First</h2>
+                    <p>I've tried this for both row, and span12, but my table inside span12, inside row always sits left. Adding padding moves it to the center, but I want to center it in a responsive way. Can I do auto padding somehow?</p>
+                </div>
+
+                <div class="col-md-4 col-md-offset-4 border-light">
+                    <h2>First</h2>
+                    <p>I've tried this for both row, and span12, but my table inside span12, inside row always sits left. Adding padding moves it to the center, but I want to center it in a responsive way. Can I do auto padding somehow?</p>
+                </div>
             </div>
-        </div>
-        <div class="row">
-                <h2>First</h2>
-                <p>I've tried this for both row, and span12, but my table inside span12, inside row always sits left. Adding padding moves it to the center, but I want to center it in a responsive way. Can I do auto padding somehow?</p>
         </div>
     </div>
 </template>
---
+++ app/static/app/js/components/MainFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="footer">
+        <div class="container">
+            <p>Toni Domazet</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "MainFooter"
+    };
+
+</script>
