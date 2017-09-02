03bb30181e3934aba65b159853f49fe2131b756f
tdomazet93 <toni.domaz93@hotmail.com>
2017-09-02 16:25:36 +0200

    add succes and error alert on server response status

--- app/static/app/js/components/Contact.vue
+++ app/static/app/js/components/Contact.vue
@@ -31,9 +31,13 @@
         <div class="row">
             <div class="col-md-4 contact-body">
                 <input type="button" class="btn" value="Get in touch" @click="onSubmit()">
-                <div v-if="isInvalidForm" class="error">
-                    <p>Invalid form</p>
-                    <a href="#" class="close-btn" @click.prevent="isInvalidForm=false"><img src="~images/close.svg"></a>
+                <div v-if="isError" class="error alert">
+                    <p>Something goes wrong. Check your form again!</p>
+                    <a href="#" class="close-btn" @click.prevent="isError=false"><img src="~images/close.svg"></a>
+                </div>
+                <div v-if="isSuccessMail" class="success alert">
+                    <p>Mail sent successfully. We will be in touch shortly!</p>
+                    <a href="#" class="close-btn" @click.prevent="isSuccessMail=false"><img src="~images/close.svg"></a>
                 </div>
             </div>
         </div>
@@ -54,32 +58,32 @@
                     msg: ""
                 },
                 emailValidator,
-                isInvalidForm: false,
+                isError: false,
+                isSuccessMail: false,
                 URLstring: ""
             };
         },
         methods: {
             onSubmit() {
-                if(this.checkIsValidForm()) {
-                    this.setURLString();
-                    this.$http.post(`${this.URLstring}/contact/`, this.formData);
-                }
-            },
-            setURLString() {
+                this.isError = false;
+                this.isSuccessMail = false;
+
+
                 if(process.env.NODE_ENV === "development") {
                     this.URLstring = `${config.ROOT_URL}:${config.PORT}`;
                 } else if(process.env.NODE_ENV === "production") {
                     this.URLstring = config.ROOT_URL;
                 }
-            },
-            checkIsValidForm() {
-                if(this.formData.fullname.length < 2) {
-                    return this.isInvalidForm = true;
-                }
-                if(emailValidator(this.formData.email) === null) {
-                    return this.isInvalidForm = true;
-                }
-                this.isInvalidForm = false;
+
+
+                this.$http.post(`${ this.URLstring }/contact/`, this.formData)
+                .then(res => {
+                    this.isSuccessMail = true;
+                })
+                .catch(err => {
+                    this.isError = true;
+                });
+
             }
         },
         components: {
